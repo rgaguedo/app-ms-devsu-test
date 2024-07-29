b1269004cd6460e035ecd12528605e9486e55c33
Raulitho <rgaguedo@gmail.com>
2024-07-28 23:25:06 -0500

    Se añade ClienteService con CRUD básico

---
+++ ms-cliente-persona-api/src/main/java/com/devsu/ms_cliente_persona_api/service/ClienteService.java
@@ -0,0 +1,52 @@
+package com.devsu.ms_cliente_persona_api.service;
+
+import com.devsu.ms_cliente_persona_api.model.Cliente;
+import com.devsu.ms_cliente_persona_api.exception.ResourceNotFoundException;
+import com.devsu.ms_cliente_persona_api.repository.ClienteRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ClienteService {
+
+    private ClienteRepository clienteRepository;
+
+    public ClienteService(ClienteRepository clienteRepository) {
+        this.clienteRepository = clienteRepository;
+    }
+
+    public Cliente createCliente(Cliente cliente) {
+        return clienteRepository.save(cliente);
+    }
+
+    public Cliente updateCliente(Long id, Cliente clienteDetails) {
+        Cliente cliente = clienteRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Cliente no encontrado para este id :: " + id));
+
+        cliente.setNombre(clienteDetails.getNombre());
+        cliente.setClave(clienteDetails.getClave());
+        cliente.setEstado(clienteDetails.isEstado());
+        // Otros setters para las propiedades de Persona
+
+        return clienteRepository.save(cliente);
+    }
+
+    public ResponseEntity<?> deleteCliente(Long id) {
+        Cliente cliente = clienteRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Cliente no encontrado para este id :: " + id));
+
+        clienteRepository.delete(cliente);
+        return ResponseEntity.ok().build();
+    }
+
+    public List<Cliente> getAllClientes() {
+        return clienteRepository.findAll();
+    }
+
+    public Cliente getClienteById(Long id) {
+        return clienteRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Cliente no encontrado para este id :: " + id));
+    }
+}
