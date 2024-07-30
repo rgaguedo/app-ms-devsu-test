e82a75624a0799d7ad5df22bb3bf0287bc90bb79
Raulitho <rgaguedo@gmail.com>
2024-07-29 23:53:40 -0500

    Se limpió el código en las clases main.

--- ms-cliente-persona-api/src/main/java/com/devsu/ms_cliente_persona_api/MsClientePersonaApiApplication.java
+++ ms-cliente-persona-api/src/main/java/com/devsu/ms_cliente_persona_api/MsClientePersonaApiApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MsClientePersonaApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MsClientePersonaApiApplication.class, args);
 	}
-
 }
--- ms-cuenta-movimientos-api/src/main/java/com/devsu/ms_cuenta_movimientos_api/MsCuentaMovimientosApiApplication.java
+++ ms-cuenta-movimientos-api/src/main/java/com/devsu/ms_cuenta_movimientos_api/MsCuentaMovimientosApiApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MsCuentaMovimientosApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MsCuentaMovimientosApiApplication.class, args);
 	}
-
 }
