476cfafb94a39b098ff3ddacddb86fd747a43785
Raulitho <rgaguedo@gmail.com>
2024-07-29 23:58:19 -0500

    Se eliminó pruebas vacías de carga de contexto

--- ms-cliente-persona-api/src/test/java/com/devsu/ms_cliente_persona_api/MsClientePersonaApiApplicationTests.java
+++ ms-cliente-persona-api/src/test/java/com/devsu/ms_cliente_persona_api/MsClientePersonaApiApplicationTests.java
@@ -1,13 +1,8 @@
 package com.devsu.ms_cliente_persona_api;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MsClientePersonaApiApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
--- ms-cuenta-movimientos-api/src/test/java/com/devsu/ms_cuenta_movimientos_api/MsCuentaMovimientosApiApplicationTests.java
+++ ms-cuenta-movimientos-api/src/test/java/com/devsu/ms_cuenta_movimientos_api/MsCuentaMovimientosApiApplicationTests.java
@@ -1,13 +1,8 @@
 package com.devsu.ms_cuenta_movimientos_api;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MsCuentaMovimientosApiApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
